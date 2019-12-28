f13afe95786d120070dac5ea9f4395591aee02b1
Brian Sinnicke <briansinnicke@gmail.com>
2019-12-28 17:28:29 -0500

    refactor thumbnail selector

--- README.md
+++ README.md
@@ -1,31 +1,57 @@
-## react-native-thumbnail-selector
-[![Platform](https://img.shields.io/badge/platform-react--native-lightgrey.svg)](http://facebook.github.io/react-native/) [![npm version](http://img.shields.io/npm/v//react-native-thumbnail-selector.svg)](https://www.npmjs.com/package/react-native-thumbnail-selector) [![npm version](http://img.shields.io/npm/dm//react-native-thumbnail-selector.svg)](https://www.npmjs.com/package/react-native-thumbnail-selector) [![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.github.com/testshallpass/react-native-thumbnail-selector/master/LICENSE)
+# react-native-thumbnail-selector
 
- **Requires react-native >=v0.43.0**
+[![Platform](https://img.shields.io/badge/platform-react--native-lightgrey.svg)](http://facebook.github.io/react-native/)
+![npm](https://img.shields.io/npm/v/react-native-thumbnail-selector)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.github.com/testshallpass/react-native-thumbnail-selector/master/LICENSE)
 
-A Flatlist thumbnail selector that animates from the bottom by default.
+A thumbnail selector that internally uses Flatlist component so it requires react-native 0.43.0 or higher.
 
-### Installation
+## Installation
+
 ```npm i react-native-thumbnail-selector --save```
 
-### Demo
-![screenshot](https://raw.github.com/testshallpass/react-native-thumbnail-selector/master/screenshots/demo.gif)
+## Usage
 
-### Props
+```javascript
+import ThumbnailSelector from 'react-native-thumbnail-selector';
+const REACT_NATIVE_LOGO = 'https://facebook.github.io/react-native/docs/assets/favicon.png';
+export default class App extends Component {
+  componentDidMount() {
+    this._show();
+  }
+  _show = () => {
+    // thumbnail objects must adhere to having image and caption properties.
+    const thumbnails = [{ image: REACT_NATIVE_LOGO, caption: 'Logo' }];
+    this.thumbnailSelectorRef.show(thumbnails);
+  };
+  _hide = () => {
+    this.thumbnailSelectorRef.hide();
+  };
+  _onSelect = item => {
+    this._hide();
+  };
+  render() {
+    return (
+      <ThumbnailSelector
+        ref={ref => this.thumbnailSelectorRef = ref}
+        onSelect={this._onSelect}
+      />
+    );
+  }
+}
+```
+
+## Props
+
 | Name | Type | Description | Default |
 | --- | :---: | --- | --- |
-| ```items``` | **Required** Array of objects with key  | (i.e.`[{key: 0, title: 'Brian', borderColor: 'white', imageUri: 'http://path/to/image.png', selected: false}]`) | []
-| ```visible``` | Bool  | show or hide selector | false
-| ```backgroundColor``` | String  | backgroundColor of Flatlist | false
-| ```loadMore``` | Func  | Callback to load more items at the end of the list | null
-| ```flatlistProps``` | Func  | `<Flatlist />` props | null
-| ```opacity``` | Number  | unselected items opacity | 0.8
-| ```onSelectedItem``` | Func  | Invoked when user selects an item | null
-| ```closeOnSelect``` | Bool  | whether or not to close after item is selected | true
-| ```zIndex``` | Number  | zIndex | 1000
-| ```closeOnSelectInterval``` | Number  | duration of close animation | 200
-| ```numberOfLines``` | Number  | numberOfLines for caption | 2
-| ```captionTextStyle``` | Text.proptypes.style | caption text style | `{color: 'white', fontFamily: 'Avenir', fontSize: 16, textAlign: 'center'}`
-| ```thumbnailImageStyle``` | Image.proptypes.style | thumbnail image style | `{width: 125, height: 125, borderWidth: 2, borderRadius: 2}`
-| ```containerStyle``` | ViewPropTypes.style | animated view style | `{position: 'absolute', bottom: 0}`
-| ```itemContainerStyle``` | ViewPropTypes.style | item view style | `{flexDirection: 'column', paddingLeft: 8, paddingRight: 8, paddingTop: 8, alignItems: 'center}`
+| ```containerStyle``` | Object | animated view style | `{ backgroundColor: 'gray' }`
+| ```thumbnailStyle``` | Object | thumbnail style | `{ flexDirection: 'column', margin: 8, alignItems: 'center' }`
+| ```imageStyle``` | Object | image style | `{ width: 125, height: 125, borderWidth: 2 }`
+| ```captionStyle``` | Object | caption text style | `{ color: 'white', fontSize: 16, textAlign: 'center', fontWeight: 'bold' }`
+| ```animationDuration``` | Number  | how long it animates show and hide | 600
+| ```onSelect``` | Func  | invoked at thumbnail selection | `() => {}`
+| ```activeOpacity``` | Number  | selected thumbnails opacity | 1
+| ```inactiveOpacity``` | Number  | unselected thumbnails opacity | 0.5
+| ```activeBorderColor``` | String  | selected thumbnails borderColor | transparent
+| ```inactiveBorderColor``` | String  | unselected thumbnails borderColor | white
--- ThumbnailSelector.js
+++ ThumbnailSelector.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
   Text,
   FlatList,
@@ -6,256 +7,159 @@ import {
   TouchableOpacity,
   Animated,
   Dimensions,
-  ViewPropTypes,
   View,
 } from 'react-native';
-import PropTypes from 'prop-types';
-const {height, width} = Dimensions.get('window');
 const WINDOW = Dimensions.get('window');
+const HEIGHT = WINDOW.height;
 
 export default class ThumbnailSelector extends Component {
   static propTypes = {
-    items: PropTypes.array.isRequired,
-    opacity: PropTypes.number,
-    backgroundColor: PropTypes.string,
-    onSelectedData: PropTypes.func,
-    closeOnSelect: PropTypes.bool,
-    zIndex: PropTypes.number,
-    closeOnSelectInterval: PropTypes.number,
-    numberOfLines: PropTypes.number,
-    visible: PropTypes.bool,
-    captionTextStyle: Text.propTypes.style,
-    thumbnailImageStyle: Image.propTypes.style,
-    flatlistProps: PropTypes.func,
-    containerStyle: ViewPropTypes.style,
-    itemContainerStyle: ViewPropTypes.style,
-    loadMore: PropTypes.func,
+    containerStyle: PropTypes.object,
+    thumbnailStyle: PropTypes.object,
+    imageStyle: PropTypes.object,
+    captionStyle: PropTypes.object,
+    animationDuration: PropTypes.number,
+    onSelect: PropTypes.func,
+    activeOpacity: PropTypes.number,
+    inactiveOpacity: PropTypes.number,
+    activeBorderColor: PropTypes.string,
+    inactiveBorderColor: PropTypes.string,
   };
   static defaultProps = {
-    items: null,
-    opacity: 0.8,
-    backgroundColor: 'dimgray',
-    onSelectedData: null,
-    closeOnSelect: true,
-    zIndex: 1000,
-    closeOnSelectInterval: 200,
-    numberOfLines: 2,
-    visible: false,
-    loadMore: null,
     containerStyle: {
-      position: 'absolute',
-      bottom: 0,
+      backgroundColor: 'gray'
     },
-    itemContainerStyle: {
+    thumbnailStyle: {
       flexDirection: 'column',
-      paddingLeft: 8,
-      paddingRight: 8,
-      paddingTop: 8,
+      margin: 8,
       alignItems: 'center',
     },
-    captionTextStyle: {
-      color: 'white',
-      fontFamily: 'Avenir',
-      fontSize: 16,
-      textAlign: 'center',
-    },
-    thumbnailImageStyle: {
+    imageStyle: {
       width: 125,
       height: 125,
       borderWidth: 2,
-      borderRadius: 2,
-      borderColor: 'white',
     },
-    flatlistProps: null,
+    captionStyle: {
+      color: 'white',
+      fontSize: 16,
+      textAlign: 'center',
+      fontWeight: 'bold',
+    },
+    animationDuration: 600,
+    onSelect: () => {},
+    activeOpacity: 1,
+    inactiveOpacity: 0.5,
+    activeBorderColor: 'white',
+    inactiveBorderColor: 'transparent',
   };
   constructor(props) {
     super(props);
-    if (props.items && props.items.length === 0) {
-      console.warn('ThumbnailSelector Items prop: Requires least one item.');
-    }
     this.state = {
-      fadeAnim: new Animated.Value(0),
-      items: props.items,
-      visible: false,
-      startDelta: WINDOW.height + 155,
+      animationValue: new Animated.Value(0),
+      thumbnails: [],
+      selectedIndex: -1,
+      startDelta: HEIGHT,
       endDelta: 0,
-      duration: 600,
     };
   }
-  componentDidMount() {
-    const {visible} = this.props;
-    if (visible) {
-      this.show();
+  show = (thumbnails = []) => {
+    if (Array.isArray(thumbnails) && thumbnails.length > 0) {
+      this.setState({ thumbnails });
+      this._animate(1);
     }
-  }
-  componentWillReceiveProps(nextProps) {
-    const {visible, items} = this.props;
-    if (nextProps.visible !== visible) {
-      if (nextProps.visible) {
-        this.show();
-      } else {
-        this.hide();
-      }
-    }
-    if (nextProps.items.length > items.length) {
-      this.setState({
-        items: nextProps.items,
+  };
+  hide = () => { 
+    this._animate(0, () => {
+      this.setState({ 
+        thumbnails: [], 
+        selectedIndex: -1,
       });
-    }
-  }
-  show = () => {
-    this.animateToValue(1);
-    this.scrollToSelected();
-  };
-  hide = () => {
-    this.timeOutForisVisible(false);
-    this.animateToValue(0);
-  };
-  animateToValue = toValue => {
-    Animated.timing(this.state.fadeAnim, {
-      toValue: toValue,
-      duration: this.state.duration,
-      friction: 7,
-    }).start();
-  };
-  timeOutForisVisible = visible => {
-    setTimeout(
-      function() {
-        this.setState({
-          visible: visible,
-        });
-      }.bind(this),
-      this.state.duration,
-    );
-  };
-  scrollToSelected = () => {
-    var index = 0;
-    const {items} = this.state;
-    for (var i = 0; i < items.length; i++) {
-      var item = items[i];
-      if (item.selected) {
-        index = i;
-        break;
-      }
-    }
-    if (index > 0) {
-      this.flatList.scrollToIndex({index: index});
-    }
-  };
-  onLayoutEvent = event => {
-    const {x, y, width, height} = event.nativeEvent.layout;
-    this.setState({
-      startDelta: WINDOW.height + height,
-      endDelta: 0,
     });
   };
-  itemAction = (item, index) => {
-    var items = this.state.items;
-    for (const value of items) {
-      value.selected = false;
-    }
-    item.selected = true;
-    this.setState({
-      items: items,
-    });
-    if (this.props.onSelectedItem) {
-      this.props.onSelectedItem(item);
-    }
-    if (this.props.closeOnSelect == true) {
-      setTimeout(
-        function() {
-          this.hide();
-        }.bind(this),
-        this.props.closeOnSelectInterval,
-      );
+  _animate = (toValue, onComplete = () => {}) => {
+    Animated.timing(this.state.animationValue, {
+      toValue,
+      duration: this.props.animationDuration,
+      friction: 9,
+    }).start(onComplete);
+  };
+  _onLayout = event => {
+    const { height } = event.nativeEvent.layout;
+    if (height != this.state.startDelta) {
+      this.setState({ startDelta: HEIGHT + height });
     }
   };
-  onEndReached = () => {
-    const {flatlistProps, loadMore} = this.props;
-    if (flatlistProps) {
-      if (flatlistProps.onEndReached) {
-        flatlistProps.onEndReached();
-      }
-      return;
-    }
-    if (loadMore) {
-      loadMore();
-    }
+  _onSelect = (item, index) => {
+    this.setState({ selectedIndex: index });
+    this.props.onSelect(item);
   };
-  renderItem = ({item, index}) => {
-    const {
-      itemContainerStyle,
-      captionTextStyle,
-      thumbnailImageStyle,
-      numberOfLines,
-      opacity,
-    } = this.props;
+  _renderItem = ({ item, index }) => {
+    const { selectedIndex } = this.state;
+    const { thumbnailStyle, captionStyle, imageStyle, activeOpacity, inactiveOpacity, activeBorderColor, inactiveBorderColor } = this.props;
+    const selected = selectedIndex == index;
+    const opacity = selected ? activeOpacity : inactiveOpacity;
+    const borderColor = selected ? activeBorderColor : inactiveBorderColor;
+    const { image, caption } = item;
     return (
-      <TouchableOpacity onPress={() => this.itemAction(item, index)}>
+      <TouchableOpacity onPress={() => this._onSelect(item, index)}>
         <View
-          style={[itemContainerStyle, {opacity: item.selected ? 1 : opacity}]}
+          style={[thumbnailStyle, { opacity }]}
         >
-          {this.renderImage(item.imageUri, thumbnailImageStyle, item.selected)}
-          {this.renderText(
-            item.title,
-            captionTextStyle,
-            numberOfLines,
-            item.selected,
-          )}
+          {image && this._renderImage(image, imageStyle, borderColor)}
+          {caption && this._renderText(caption, captionStyle, selected)}
         </View>
       </TouchableOpacity>
     );
   };
-  renderImage(imageUri, style, selected) {
+  _renderImage = (uri, style, borderColor) => {
     return (
       <Image
-        style={[
-          style,
-          {borderColor: selected ? style.borderColor : 'transparent'},
-        ]}
-        source={{uri: imageUri}}
+        style={[ style, { borderColor } ]}
+        source={{ uri }}
+        resizeMode={'contain'}
       />
     );
-  }
-  renderText(text, style, numberOfLines, selected) {
+  };
+  _renderText = (text, style, selected) => {
+    let fontWeight = 'normal';
+    if (selected && style.fontWeight) {
+      fontWeight = style.fontWeight;
+    }
     return (
-      <Text
-        style={[style, {fontWeight: selected ? 'bold' : 'normal'}]}
-        numberOfLines={numberOfLines}
-      >
+      <Text style={ [style, { fontWeight }] }>
         {text}
       </Text>
     );
-  }
+  };
   render() {
-    const {startDelta, endDelta, fadeAnim, dataSource, items} = this.state;
-    const {zIndex, backgroundColor, flatlistProps, containerStyle} = this.props;
+    const { startDelta, endDelta, animationValue, thumbnails } = this.state;
+    const { containerStyle } = this.props;
+    const wrapperStyle = [
+      containerStyle,
+      {
+        transform: [
+          {
+            translateY: animationValue.interpolate({
+              inputRange: [0, 1],
+              outputRange: [startDelta, endDelta],
+            }),
+          },
+        ],
+        elevation: 1,
+      },
+    ];
     return (
       <Animated.View
-        style={[
-          containerStyle,
-          {
-            zIndex: zIndex,
-            transform: [
-              {
-                translateY: fadeAnim.interpolate({
-                  inputRange: [0, 1],
-                  outputRange: [startDelta, endDelta],
-                }),
-              },
-            ],
-          },
-        ]}
-      >
+        onLayout={this._onLayout}
+        style={wrapperStyle}>
         <FlatList
           ref={ref => this.flatList = ref}
-          style={{backgroundColor: backgroundColor, flex: 1}}
-          data={items}
+          data={thumbnails}
+          extraData={this.state}
           horizontal={true}
-          renderItem={this.renderItem}
-          onEndReached={this.onEndReached}
-          keyExtractor={item => item.key}
-          {...flatlistProps}
+          renderItem={this._renderItem}
+          keyExtractor={(item, index) => `${index}`}
+          {...this.props}
         />
       </Animated.View>
     );
