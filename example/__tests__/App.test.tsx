22a9fe271a5236f8059472b2ede9fb69c0fb3163
testshallpass <testshallpass@gmail.com>
2023-08-06 11:08:07 -0400

    upgrade example react-native to 0.72.3

--- example/__tests__/App.test.tsx
+++ example/__tests__/App.test.tsx
@@ -9,6 +9,10 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+// Note: import explicitly to use the types shiped with jest.
+import {it} from '@jest/globals';
+
 it('renders correctly', () => {
-  renderer.create(<App />);
+ const component = renderer.create(<App />);
+ expect(component.toJSON()).toMatchSnapshot();
 });
